78dc69e14cc60eacef0119b4b6fe7f4710048c52
bcattaneo <c@ttaneo.uy>
2019-08-04 20:07:55 -0300

    Add chat icon to page

---
+++ firefox/instagram-chat.js
@@ -0,0 +1,12 @@
+const iconsClassName = '_47KiJ';
+const chat = `
+    <div class="mXkkY KDuQp">
+        <a class="xWeGp" href="/direct/inbox/">
+            <span class="glyphsSpriteDirect__outline__24__grey_9 u-__7" aria-label="Direct"></span>
+        </a>
+    </div>
+`;
+
+// Add chat icon to page
+var icons = document.getElementsByClassName(iconsClassName)[0];
+icons.innerHTML = chat + icons.innerHTML;
